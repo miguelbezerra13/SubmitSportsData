5ce24ee5ef93f5f395b843862b9a7784e41d102d
Miguel Bezerra <miguel.bezerra13@gmail.com>
2022-02-02 22:18:03 +0000

    Connection to the API from JS and API Development
    - Create the structure and the values for the JSON and Post request to API
    - Development of the API for the Post request
    - Minor adjustments in the HTML and CSS

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Declare the variables
 const activityForm = document.querySelector('#activity-form');
-const activities = document.getElementsByName('activity')
-const dateString = document.querySelector('#date')
-const distance = document.querySelector('#distance')
-const hours = document.querySelector('#hours')
-const minutes = document.querySelector('#minutes')
-const seconds = document.querySelector('#seconds')
-const calories = document.querySelector('#calories')
-const elevation = document.querySelector('#elevation')
+const activities = document.getElementsByName('activity');
+const dateString = document.querySelector('#date');
+const distance = document.querySelector('#distance');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+const calories = document.querySelector('#calories');
+const elevation = document.querySelector('#elevation');
 
 activityForm.addEventListener('submit', onSubmit);
 
@@ -16,7 +16,7 @@ function onSubmit(e) {
     e.preventDefault();
 
     // Get the selected radio button
-    let activity
+    let activity;
 
     for (i=0; i < activities.length; i++) {
         if(activities[i].checked)
@@ -24,15 +24,27 @@ function onSubmit(e) {
     };
 
     // Format the date
-    const date = new Date(dateString.value).toLocaleDateString("en-US")
+    const date = new Date(dateString.value).toLocaleDateString("en-US");
 
-    console.log(activity.value)
-    console.log(date)
-    console.log(distance.value)
-    console.log(hours.value)
-    console.log(minutes.value)
-    console.log(seconds.value)
-    console.log(calories.value)
-    console.log(elevation.value)
+    // Create the structure of the JSON and add the corresponding values
+    const actLog = {
+        "activityType": activity.value,
+        "activityDate": date,
+        "activityDistance": distance.value,
+        "activityHours": hours.value,
+        "activityMinutes": minutes.value,
+        "activitySeconds": seconds.value,
+        "activityCalories": calories.value,
+        "activityElevation": elevation.value
+    };
 
-}
+    // Send the data to the API
+    fetch('http://localhost:8000/post', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(actLog),
+    })
+
+    // The only thing missing would be a sign in the web page that the data was correctly sent
+
+};
---
+++ main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+
+from datetime import datetime as dt
+
+import pandas as pd
+
+app = FastAPI()
+
+origins = ["http://localhost", "http://localhost:5500"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"])
+
+# Expected JSON format to be received from front
+class ActivityLog(BaseModel):
+    activityType: str
+    activityDate: str
+    activityDistance: float 
+    activityHours: int
+    activityMinutes: int
+    activitySeconds: int
+    activityCalories: Optional[str] = None
+    activityElevation: Optional[str] = None
+
+@app.post("/post")
+def post_activity_log(activity_log: ActivityLog):
+    
+    # Transform the date into a datetime to extract the year and month
+    log_date = dt.strptime(activity_log.activityDate, '%m/%d/%Y')
+
+    log_year = log_date.year
+    log_month = log_date.month
+
+    # Convert the activity duration inputs into hours
+    time_h = activity_log.activityHours + (activity_log.activityMinutes/60) + (activity_log.activitySeconds/3600)
+    
+    # Calculate the average speed
+    avg_speed = activity_log.activityDistance / time_h
+
+    # Preparing the data t%Y-%m-%do add to the csv
+    # Get the last index of the dataset
+    last_csv_idx = (pd.read_csv('rwc.csv', usecols=[0], index_col=0).index)[-1]+1
+
+    # Make a dictionary to create a dataframe
+    line = {
+        "index": [last_csv_idx],
+        "Date": [activity_log.activityDate],
+        "Type": [activity_log.activityType],
+        "Distance_km": [activity_log.activityDistance],
+        "Hours": [activity_log.activityHours],
+        "Minutes": [activity_log.activityMinutes],
+        "Seconds": [activity_log.activitySeconds],
+        "Time_h": [time_h],
+        "Calories": [activity_log.activityCalories],
+        "ElevGain_m": [activity_log.activityElevation],
+        "AvgSpeed_km/h": [avg_speed],
+        "Year": [log_year],
+        "Month": [log_month]
+    }
+
+    # Create the dataframe and set the index
+    test_df = pd.DataFrame(line).set_index('index')
+
+    # Add the log to the end of the dataset
+    test_df.to_csv('rwc.csv', mode='a', header=False)
